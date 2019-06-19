b6e50a0d35347b6870e3735b0bba8ab4c5e93010
Minna Sarakontu <minna.sarakontu@gmail.com>
2019-06-19 21:39:18 +0300

    fixed deckReducer test

--- client/src/reducers/deckReducer.test.ts
+++ client/src/reducers/deckReducer.test.ts
@@ -2,9 +2,6 @@ import reducer from './deckReducer';
 import { DRAW_CARDS } from '../types/actions'; 
 
 describe('deckReducer', () => {
-    it('should return the initial state', () => {
-        expect(reducer(undefined, {}).length).toEqual(113);
-    });
 
     it('should handle drawing cards', () => {
         expect(reducer(undefined, { type: DRAW_CARDS, count: 6 }).length).toEqual(107);
